55032a4f748471c0e05f40118f7aa29f76a2c761
rrrtt001 <101165023+rrrtt001@users.noreply.github.com>
2022-08-29 11:01:21 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -24,3 +24,4 @@ DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo ">>> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy.log
 nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ec2-user/deploy_err.log &
 
+
